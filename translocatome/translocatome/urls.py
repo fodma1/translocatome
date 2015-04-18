d2047da288f7deb37aa6571edcba12614b3a1713
Matyas Fodor <matyas.fodor@prezi.com>
2015-04-18 20:41:09 +0200

    [users] login/register logic added

--- translocatome/translocatome/urls.py
+++ translocatome/translocatome/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
     # Examples:
@@ -7,4 +8,13 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    # User management
+    url(r'^accounts/login/$',  'translocatome.views.login'),
+    url(r'^accounts/auth/$',  'translocatome.views.auth_view'),
+    url(r'^accounts/logout/$', 'translocatome.views.logout'),
+    url(r'^accounts/loggedin/$', 'translocatome.views.loggedin'),
+    url(r'^accounts/invalid/$', 'translocatome.views.invalid_login'),
+    url(r'^accounts/register/$', 'translocatome.views.register_user'),
+    url(r'^accounts/register_success/$', 'translocatome.views.register_success'),
 ]
