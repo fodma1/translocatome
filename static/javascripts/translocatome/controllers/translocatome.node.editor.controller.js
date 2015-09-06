962cf02ae181dd587cd01a03028adaad634997f4
Matyas Fodor <matyas.fodor@prezi.com>
2015-09-06 21:30:46 +0200

    [frontend] add node editor templates and logic

--- static/javascripts/translocatome/controllers/translocatome.node.editor.controller.js
+++ static/javascripts/translocatome/controllers/translocatome.node.editor.controller.js
@@ -5,14 +5,55 @@
         .module('thinkster.translocatome.controllers')
         .controller('TranslocatomeNodeEditorController', TranslocatomeNodeEditorController);
 
-    TranslocatomeNodeEditorController.$inject = ['$scope', '$http'];
+    TranslocatomeNodeEditorController.$inject = ['$scope', '$http', 'growl'];
 
-    function TranslocatomeNodeEditorController($scope, $http) {
+    function TranslocatomeNodeEditorController($scope, $http, growl) {
 
         function init()
         {
-            $scope.displayText = 'This text should be displayed!';
+            $scope.node = $scope.ngDialogData.node || {};
+            $scope.loading = false;
         }
+
+        $scope.deleteNode = function() {
+            $scope.loading = true;
+            if (confirm('Are you sure you want to delete this node?')) {
+                $http.delete('api/translocatome/rest-api/node/' + $scope.node.id + '/')
+                    .then(function() {
+                        growl.addSuccessMessage('successfully deleted');
+                    }, function(response) {
+                        growl.addErrorMessage('Errors: ' + response.data);
+                    })
+                    .finally(function() {
+                        $scope.loading = false;
+                    });
+            }
+        };
+
+        $scope.save = function() {
+            $scope.loading = true;
+            if ($scope.node.id) {
+                $http.put('api/translocatome/rest-api/node/' + $scope.node.id + '/', $scope.node)
+                    .then(function() {
+                        growl.addSuccessMessage('successfully updated');
+                    }, function(response) {
+                        growl.addErrorMessage('Errors: ' + response.data);
+                    })
+                    .finally(function() {
+                        $scope.loading = false;
+                    });
+            } else {
+                $http.post('api/translocatome/rest-api/node/', $scope.node)
+                    .then(function() {
+                        growl.addSuccessMessage('successfully updated');
+                    }, function(response) {
+                        growl.addErrorMessage('Errors: ' + response.data);
+                    })
+                    .finally(function() {
+                        $scope.loading = false;
+                    });
+            }
+        };
 
         init();
     }
