e93e96a29fc389edbf156401446397de3287d229
Matyas Fodor <matyas.fodor@prezi.com>
2015-09-06 21:31:04 +0200

    [backend] minor fixes

--- translocatome/rest_api.py
+++ translocatome/rest_api.py
@@ -29,6 +29,7 @@ def node(request, node_id=None):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(str(serializer.errors), status=status.HTTP_400_BAD_REQUEST)
 
     try:
         node_object = Node.objects.get(id=node_id)
@@ -48,4 +49,4 @@ def node(request, node_id=None):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(str(serializer.errors), status=status.HTTP_400_BAD_REQUEST)
--- translocatome/urls.py
+++ translocatome/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns
 
 urlpatterns = patterns('translocatome.views',
                        (r'query-nodes/$', 'query_nodes'),
-                       (r'query-interactions/$', 'query_interactions')
+                       (r'query-interactions/$', 'query_interactions'),
                        )
 
 urlpatterns += patterns('translocatome.rest_api',
